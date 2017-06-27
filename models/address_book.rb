ad5566c30e1214a20015d62f83d551148d0added
Mark Brooks <Markabrooks@gmail.com>
2017-06-26 19:37:39 -0600

    Checkpoint 7 Searching: Complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -30,6 +30,26 @@ require "csv"
     end
   end
 
+  #Search AddressBook for a specific entry by name
   def binary_search(name)
+    # #1
+      lower = 0
+      upper = entries.length - 1
+
+      while lower <= upper
+        # #3
+        mid = (lower + upper) / 2
+        mid_name = entries[mid].name
+
+        if name == mid_name
+          return entries[mid]
+        elsif name < mid_name
+          upper = mid - 1
+        elsif name > mid_name
+          lower = mid + 1
+        end
+      end
+      return nil
   end
+
 end
